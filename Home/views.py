8131611c737a9ad145d2cd797c98a1bcc89f2ed2
amanash <amanmahore.ash@gmail.com>
2021-05-21 15:39:31 +0530

    portfolio

---
+++ Home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from Home.models import Contact
+
+# Create your views here.
+
+
+def Home(request):
+    if request.method == "POST":
+
+        name = request.POST['name']
+        email = request.POST['email']
+        number = request.POST['phone']
+        desc = request.POST['message']
+        #print(name, email, phone, desc)
+        ins = Contact(name=name, number=number, email=email, desc=desc)
+        ins.save()
+        print("the data has been written inside the database")
+
+    return render(request, 'home.html')
